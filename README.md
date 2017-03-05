7bb7277339b4967e3f9924bd823f171aefa43f4b
Jesse Adelman <jesse@boldandbusted.com>
2017-03-04 20:05:35 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple_ip_service
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'simple_ip_service'
+maintainer 'The Authors'
+maintainer_email 'jesse@boldandbusted.com'
+license 'gplv3'
+description 'Installs/Configures simple_ip_service'
+long_description 'Installs/Configures simple_ip_service'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/simple_ip_service/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/simple_ip_service' if respond_to?(:source_url)
