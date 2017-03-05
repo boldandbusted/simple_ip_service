736b0ede647c8ebf5364756675c4d81fcb70af0a
Jesse Adelman <jesse@boldandbusted.com>
2017-03-05 00:19:24 -0500

    First commit of working code. My comments are usually much more verbose in practice

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jesse@boldandbusted.com'
 license 'gplv3'
 description 'Installs/Configures simple_ip_service'
 long_description 'Installs/Configures simple_ip_service'
-version '0.1.0'
+version '0.1.1'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -17,3 +17,8 @@ version '0.1.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/simple_ip_service' if respond_to?(:source_url)
+#
+
+depends "chef_nginx"
+depends "yum"
+depends "poise-service"
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+include_recipe "chef_nginx::default"
+include_recipe "yum::default" if node[:platform]=='centos' || node[:platform]=='redhat'
+include_recipe "apt::default" if node[:platform]=='debian' || node[:platform]=='ubuntu'
+
+if node[:platform]=='centos' || node[:platform]=='redhat'
+  yum_globalconfig '/etc/yum.conf' do
+    max_retries "0"
+    action :create
+  end
+  %w{yum-priorities logwatch postfix-perl-scripts git}.each do |install|
+    yum_package install
+  end
+end
+
+package 'Install go and build tools' do
+  package_name 'golang'
+end
+
+nginx_site 'default' do
+  enable false
+end
+
+cookbook_file '/etc/nginx/sites-available/proxy_to_localhost' do
+  source 'nginx/sites-available/proxy_to_localhost'
+  action :create
+end
+
+user 'goapp' do 
+  comment 'Non-privilged user to run a Go app as'
+  manage_home true
+  uid '5000'
+  shell '/bin/bash'
+end
+
+bash 'install_go_build' do
+  user 'goapp'
+  code <<-EOH
+  export GOPATH="/home/goapp"
+  go get github.com/rdegges/ipify-api
+  EOH
+end
+
+poise_service_user 'goapp'
+
+poise_service 'ipify' do
+  command '/home/goapp/bin/ipify-api'
+  user 'goapp'
+end
+
+nginx_site 'proxy_to_localhost' do
+    enable true
+end
